685406d671fe93d10a951085a00fd56bb73ca8d4
O. HADALIN <dyblikat777@gmail.com>
2025-05-24 17:07:28 +0300

    ref: tailwind styles add to status label

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,13 @@
-import ActiveLabel from '@/app/components/ActiveLabel/ActiveLabel';
-import NotActiveLabel from '@/app/components/NotActiveLabel/NotActiveLabel';
+import StatusLabel, { Status } from '@/app/components/StatusLabel/StatusLabel';
 
 export default function Home() {
   return (
     <main>
-      <h1>Home page</h1>
-      <ActiveLabel>Activ</ActiveLabel>
-      <NotActiveLabel>Not Activ</NotActiveLabel>
+      <h1 className="text-xl">Home page</h1>
+      <StatusLabel status={Status.Active}>Activ</StatusLabel>
+      <StatusLabel status={Status.NotActive}>Not Activ</StatusLabel>
+      <StatusLabel status={Status.Pending}>Pending</StatusLabel>
+      <StatusLabel status={Status.Suspended}>Suspended</StatusLabel>
     </main>
   );
 }
