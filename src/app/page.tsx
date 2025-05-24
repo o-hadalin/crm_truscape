092382077497063c84b30e11cec566643ee01551
O. HADALIN <dyblikat777@gmail.com>
2025-05-24 16:13:49 +0300

    css: ActiveLabel, NotActiveLabel, globals

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,12 @@
+import ActiveLabel from '@/app/components/ActiveLabel/ActiveLabel';
+import NotActiveLabel from '@/app/components/NotActiveLabel/NotActiveLabel';
+
 export default function Home() {
   return (
     <main>
       <h1>Home page</h1>
+      <ActiveLabel>Activ</ActiveLabel>
+      <NotActiveLabel>Not Activ</NotActiveLabel>
     </main>
   );
 }
